eac837089012dd5a578c601c2ec561fb20f1ab68
jisungbin <ji@sungb.in>
2023-07-03 22:15:05 +0900

    Add RubberdocDsl annotation

---
+++ rubberdoc-material/src/main/kotlin/team/duckie/rubberdoc/material/dsl.kt
@@ -0,0 +1,11 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/rubberdoc-android/blob/main/LICENSE
+ */
+
+package team.duckie.rubberdoc.material
+
+@DslMarker
+public annotation class RubberdocDsl
